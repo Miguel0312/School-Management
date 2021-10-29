a3e95282fa79af5507f83ef22b4755707363af81
Miguel Pereira <miguel.vpereira14@gmail.com>
2021-10-29 03:21:47 +0200

    Now the file that stores the students is created automatically

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
-package main;
-
 import welcome.WelcomeWindow;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		try {
+      File myObj = new File("school.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File "+ myObj+ " already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
 		new WelcomeWindow();
 	}
 }
